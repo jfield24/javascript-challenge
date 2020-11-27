4ad5963340d50d19be5770b5f12bd89a08196647
Justin Field <justin_w_field@hotmail.com>
2020-11-27 20:54:20 +1100

    part one complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Fill in table with the data
+tableData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Make sure the table is empty before search
+  tbody.html("");
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  // Filter the data on datetime
+  var filteredData = tableData.filter(ufoforday => ufoforday.datetime === inputValue);
+
+  console.log(filteredData);
+
+  // Fill in table with the filtered data
+  filteredData.forEach((sightings) => {
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+};
